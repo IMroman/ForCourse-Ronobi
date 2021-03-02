6dc51805d10d9d6975889bcd45023e65481b2ac8
Ronobi <Ronobi@yandex.ru>
2021-03-02 23:45:38 +0300

    CorrectByteValueTest2

--- src/Test2/DataTypes.java
+++ src/Test2/DataTypes.java
@@ -3,7 +3,7 @@ package Test2;
 public class DataTypes {
     public static void main(String[] args) {
         double num = 16.516516516161D;//переменная типа double с наибольшим диапазоном чисел. Произвольное число double
-        byte num2 = -128;//переменная целочисленного типа с наименьшим диапазоном чисел.
+        byte num2 = 127;//переменная целочисленного типа с наименьшим диапазоном чисел.
         System.out.println(num);
         System.out.println(num2);
     }
