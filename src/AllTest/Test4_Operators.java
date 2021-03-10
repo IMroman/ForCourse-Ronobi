ae3c31e46b78f72ba9e46b6187b36fd0e56d1dae
Ronobi <Ronobi@yandex.ru>
2021-03-10 19:32:06 +0300

    Test4OperatorsAddIfElse

--- src/AllTest/Test4_Operators.java
+++ src/AllTest/Test4_Operators.java
@@ -7,8 +7,13 @@ public class Test4_Operators {
         int scanner1 = scanner.nextInt();
         int scanner2 = scanner.nextInt();
         int scanner3 = scanner.nextInt();
-        System.out.println((scanner1+scanner2+scanner3)/3);//AVG
-        double a = Math.floor((scanner1 + scanner2 + scanner3)/3) /2;
+        System.out.println((scanner1 + scanner2 + scanner3) / 3);//AVG
+        double a = Math.floor((scanner1 + scanner2 + scanner3) / 3F) / 2F;//AVG/2 + Math.floor
         System.out.println(a);//9+9+7=4.16 FACT,  4.0 Answer (Math.floor)
+        if (a > 3F) {
+            System.out.println("Программа выполнена корректно");
+        } else {
+            System.out.println("a < 3");
+        }
     }
 }
